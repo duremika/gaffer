6e3b564d877c944831fc7b5457e30eeb40565b23
Богданов Максим Владимирович <bogdanov.mv.95@gmail.com>
2022-12-12 04:00:27 +0600

    moved representative answer to local testing

--- core/src/main/java/ru/duremika/core/message/impl/MessageToUser.java
+++ core/src/main/java/ru/duremika/core/message/impl/MessageToUser.java
@@ -2,13 +2,17 @@ package ru.duremika.core.message.impl;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import ru.duremika.core.message.Message;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Getter
+@ToString
 @JsonTypeName("MESSAGE_TO_USER")
 public class MessageToUser extends Message {
     @JsonProperty
@@ -16,17 +20,4 @@ public class MessageToUser extends Message {
 
     @JsonProperty
     private final List<List<String>> buttons;
-
-    @Override
-    public String toString() {
-        return "________________________________________\n"
-                + text + (buttons == null || buttons.isEmpty() ?
-                "" :
-                "\n\n" + buttons.stream()
-                        .map(buttonsRow -> buttonsRow.stream()
-                                .map(button -> "[  " + button + "  ]")
-                                .collect(Collectors.joining(" | ")))
-                        .collect(Collectors.joining("\n")))
-                + "\n________________________________________";
-    }
 }
--- tests/src/main/java/ru/duremika/tests/LocalTesting.java
+++ tests/src/main/java/ru/duremika/tests/LocalTesting.java
@@ -2,12 +2,16 @@ package ru.duremika.tests;
 
 import ru.duremika.core.engine.Cache;
 import ru.duremika.core.engine.MessageHandler;
+import ru.duremika.core.message.Message;
 import ru.duremika.core.message.impl.MessageFromUser;
+import ru.duremika.core.message.impl.MessageToUser;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LocalTesting {
     MessageHandler messageHandler = new MessageHandler();
@@ -33,7 +37,10 @@ public class LocalTesting {
                     handleCommand(input);
                     continue;
                 }
-                messageHandler.handle(message(input));
+                Message message = messageHandler.handle(message(input));
+                if (message instanceof MessageToUser) {
+                    System.out.println(representationMessageFromUser((MessageToUser) message));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,5 +68,24 @@ public class LocalTesting {
 
     private MessageFromUser message(String messageText) {
         return new MessageFromUser(userId, messageText);
+    }
+
+    private String representationMessageFromUser(MessageToUser message) {
+        String delimiter = "__________________________________________________";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(delimiter);
+        stringBuilder.append('\n');
+        stringBuilder.append(message.getText());
+        final List<List<String>> buttons = message.getButtons();
+        stringBuilder.append((buttons == null || buttons.isEmpty() ?
+                "" :
+                "\n\n" + buttons.stream()
+                        .map(buttonsRow -> buttonsRow.stream()
+                                .map(button -> "[  " + button + "  ]")
+                                .collect(Collectors.joining("   ")))
+                        .collect(Collectors.joining("\n"))));
+        stringBuilder.append('\n');
+        stringBuilder.append(delimiter);
+        return stringBuilder.toString();
     }
 }
